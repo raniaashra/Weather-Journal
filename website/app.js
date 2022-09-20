01ec6b5ff6cf1633badb9f3dc6adb12381bd79c4
raniaashra <82020005+raniaashra@users.noreply.github.com>
2022-09-20 16:13:22 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,6 +1,9 @@
-const { url } = require("inspector");
+// const { url } = require("inspector");
+
+const { response } = require("express");
 
 /* Global Variables */
+const baseURL ="https://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = '&appid=654fdf4746b16498b5cfa06dc205d7bb&units=imperial';
 const apiUrl="http://localhost:4000";
 
@@ -8,12 +11,41 @@ const apiUrl="http://localhost:4000";
 //
 document.getElementById('generate').addEventListener('click',generated);
 function generated(){
-    let data ={zipCode: document.getElementById('zip').value,
-    content: document.getElementById('content').value,
-    date: document.getElementById('date').value
+    const zipcode = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    getWeather(baseURL, zipcode, apiKey)
+    .then(function(data){
+        console.log(data)
+        postdata('/add', {temp:data.temp, data: data.date, content:data.content})
+    });
+    
+    
+};
+const getWeather =async(baseurl, zip, key)=>{
+    const res =await fetch(baseurl+zip+key);
+    try{
+        const data =await res.json();
+        return data;
+    }catch(er){
+        console.log("error", er);
+    }
 }
-}
+const postdata = async(url ='', data ={})=>{
+    const response =await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        Headers:{'content-Type': 'package/json'},
+        
+    
+    });
+    try{
+        const newdata =await response.json();
+        return newdata;
+    }catch(er){
+        console.log("error", er);
+    }
 
+}
 const retrieveData = async () =>{
     const request = await fetch('/all');
     try {
@@ -22,7 +54,7 @@ const retrieveData = async () =>{
     console.log(allData)
     // Write updated data to DOM elements
     document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
-    document.getElementById('content').innerHTML = allData.feel;
+    document.getElementById('content').innerHTML = allData.content;
     document.getElementById("date").innerHTML =allData.date;
     }
     catch(error) {
@@ -30,6 +62,10 @@ const retrieveData = async () =>{
       // appropriately handle the error
     }
 }
+const btn =document.getElementById('generate');
+btn.addEventListener('click',function(){
+console.log("click");
+});
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
