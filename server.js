10e96b8510fb1a4c6166ae9c20f68c507ae11521
raniaashra <82020005+raniaashra@users.noreply.github.com>
2022-09-22 23:59:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,38 +5,47 @@ projectData = {};
 const express = require('express');
 // Start up an instance of app
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser =require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
-const { request } = require('express');
+
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
-const port =4000;
+const port =5500;
 const server = app.listen(port, listening);
 function listening(){
+    console.log("server running");
     console.log(`running on localhost: ${port}`);
 }
 
 app.get("/all", sendData);
-function sendData(req, res){
-    res.send(projectData);
+function sendData(request, response){
+    response.send(projectData);
+    console.log(projectData);
 }
 // POST method route
-app.post('/add', function (req, res) {
-    projectData={
-        temp:req.body.temp,
-        date:reqbody.date,
-        content:req.body.content
+let alldata =[];
+app.post('/add', function (request, response) {
+    console.log(request.body);
+    newprojectData={
+        temp:request.body.temp,
+        date:request.body.date,
+        feeling:request.body.content
     };
-    res.send(projectData);
+    response.send(projectData);
+    alldata.push(newprojectData);
 });
 
